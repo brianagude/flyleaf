65ee90af386d35bf8b5a997b4e8a85a0fbffee4d
Briana Gude <briana.gude@gmail.com>
2019-01-03 14:58:08 -0500

    javascript troubleshoot

--- src/js/global.js
+++ src/js/global.js
@@ -1,38 +1,41 @@
-// toggle the menu
-const menu = document.querySelector('nav')
-const button = document.querySelector('a.menu-button')
+document.addEventListener("DOMContentLoaded", function () {
+  // toggle the menu
+  const menu = document.querySelector('nav')
+  const button = document.querySelector('a.menu-button')
 
-  $(button).click(function(){
-    $(menu).toggleClass("show");
+    $(button).click(function(){
+      $(menu).toggleClass("show");
 
-    return false
-  });
+      return false
+    });
 
-// toggle search
-const search = document.querySelector('div.search')
-const click = document.querySelector('a.search')
+  // toggle search
+  const search = document.querySelector('div.search')
+  const click = document.querySelector('a.search')
 
-  $(click).click(function(){
-    $(search).toggleClass("show-search");
+    $(click).click(function(){
+      $(search).toggleClass("show-search");
 
-    return false
-  });
+      return false
+    });
 
-// toggle split screen
-const split = document.querySelector('section.split')
-const change = document.querySelector('a.switch')
-$(change).click(function(){
-  $(split).toggleClass("switch");
-
-  return false
-});
-
-// toggle mobile the menu
-const menu2 = document.querySelector('div.menu2')
-const button2 = document.querySelector('a.menu-button2')
-
-  $(button2).click(function(){
-    $(menu2).addClass("show");
+  // toggle split screen
+  const split = document.querySelector('section.split')
+  const change = document.querySelector('a.switch')
+  $(change).click(function(){
+    $(split).toggleClass("switch");
 
     return false
   });
+
+  // toggle mobile the menu
+  const menu2 = document.querySelector('div.menu2')
+  const button2 = document.querySelector('a.menu-button2')
+
+    $(button2).click(function(){
+      $(menu2).addClass("show");
+
+      return false
+    });
+
+})
